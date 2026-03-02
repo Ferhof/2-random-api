310ab3c5c2ee8cdf5471642ffbb0b942c4f22b11
Ferhof <alexander.kiriltsev@gmail.com>
2026-03-02 21:40:42 +0300

    Add random number generator API endpoint `/random`

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"math/rand"
+	"net/http"
+	"strconv"
+)
 
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	w.Write([]byte(strconv.Itoa(rand.Intn(6) + 1)))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	mux.HandleFunc("/random", randomHandler)
+	server.ListenAndServe()
 }
